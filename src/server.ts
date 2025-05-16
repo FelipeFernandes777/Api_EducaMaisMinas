c5a8706e49bef94253717a1d1f935dcd1fc63ee3
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-05-16 14:49:05 -0300

    fix port

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import Aplication from "./app";
 
 const server = new Aplication();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(Number(PORT), `server running on port: ${PORT}`)
